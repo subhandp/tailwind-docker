7e77747e693108363c4f3c5753b1c17070d0e2c0
subhandp <subhan.dinda.putra@gmail.com>
2020-08-14 07:17:58 +0800

    add tailwind config, and finishing

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    purge: [],
+    theme: {
+        fontFamily: {
+            display: ['lato', 'ubuntu', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', 'Helvetica Neue', 'sans-serif'],
+            body: ['lato', 'ubuntu', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', 'Helvetica Neue', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                resume: '#04143A',
+                bgJob: '#D9E7F8',
+                colorJob: '#4A90E2',
+                colorEmail: '#81899C'
+            },
+            lineHeight: {
+
+            }
+        },
+        variants: {},
+        plugins: [],
+    }
+}
